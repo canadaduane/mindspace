083fe7b4ed19f94fbd21371436cc8d5d3e665450
Duane Johnson <duane.johnson@gmail.com>
2023-10-01 21:46:45 -0600

    Add package.json and @b9g/crank locally for typechecking

--- main.js
+++ main.js
@@ -1,26 +1,19 @@
-import { renderer } from "crank/dom";
-import { jsx as html } from "crank/standalone";
+// @ts-check
+import { renderer } from "@b9g/crank/dom";
+import { jsx as html } from "@b9g/crank/standalone";
 
-function* Svg() {
-  let count = 0;
-
-  const timer = setInterval(() => {
-    count += 0.05;
-    this.refresh();
-  }, 16);
-
-  try {
-    while (true) {
-      yield html`
-        <svg viewBox="0 0 100 100">
-          <circle cx="50" cy="50" r=${count + 5} />
-        </svg>
-      `;
-    }
-  } finally {
-    clearInterval(timer);
+function* Svg({ enabled = true }) {
+  let r = 0.05;
+  while (enabled) {
+    r += 0.05;
+    requestAnimationFrame(() => this.refresh());
+    yield html`
+      <svg viewBox="0 0 100 100">
+        <circle cx="50" cy="50" r=${r} stroke />
+      </svg>
+    `;
   }
 }
 
 renderer.render(html`<${Svg} />`, document.body);
-renderer.render(html`<${Svg} />`, document.body);
+// renderer.render(html`<${Svg} />`, document.body);
