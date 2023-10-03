d01897c9be3947bfcb6eec588bb43df8201c8ca7
Duane Johnson <duane.johnson@gmail.com>
2023-10-02 22:24:09 -0600

    Add colorwheel

---
+++ colorwheel.js
@@ -0,0 +1,43 @@
+import Color from "colorjs.io";
+import { svg } from "./utils.js";
+
+export function* ColorWheel({ w, h }) {
+  const selectColor =
+    (color) =>
+    ({ clientX: x, clientY: y }) => {
+      this.dispatchEvent(
+        new CustomEvent("colorSelected", {
+          bubbles: true,
+          detail: { color, x, y },
+        })
+      );
+    };
+  for ({ w, h } of this) {
+    const size = w > h ? h : w;
+    yield svg`
+      <g transform="translate(${w / 2},${h / 2})" style="pointer-events: none">
+        ${Array.from({ length: 60 }, (_, i) => {
+          const C = 0.3;
+          const hue = ((i * 6) / 360) * Math.PI * 2 + Math.PI / 2;
+          const a = C * Math.cos(hue);
+          const b = C * Math.sin(hue);
+          const color = new Color("oklab", [1, a, b]).toString({
+            format: "rgba",
+          });
+          return svg`
+            <rect
+              onpointerdown=${selectColor(color)} 
+              y=${size * 0.42}
+              width=${size * 0.025}
+              height=${size * 0.035}
+              rx=${size * 0.01}
+              stroke="none"
+              fill=${color}
+              transform=${`rotate(${(360 * i) / 60})`}
+            />
+          `;
+        })}
+      </g>
+    `;
+  }
+}
--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 import { renderer } from "@b9g/crank/dom";
+import Color from "colorjs.io";
 import { html, svg } from "./utils.js";
+import { ColorWheel } from "./colorwheel.js";
 
 let globalIsDragging = false;
+const lineMaxDistance = 200;
+const lineTransition = 5;
+const defaultColor = "rgba(93, 103, 111, 1)";
 /**
  * nodeId: number
  * Node
@@ -23,10 +28,12 @@ let globalIsDragging = false;
  * {
  *   type: "circle"
  *   controlsNodeId: number
+ *   color: string
  *   cx: number
  *   cy: number
  * } | {
  *   type: "line"
+ *   color: string
  *   x1: number
  *   y1: number
  *   x2: number
@@ -70,20 +77,42 @@ function* Svg({ nodes: initNodes = [], shapes: initShapes = [] }) {
     if (node) {
       node.x = x;
       node.y = y;
+      this.refresh();
     } else {
       console.warn("can't set node movement", nodeId);
     }
   });
 
-  const createNode = ({ clientX: x, clientY: y }) => {
+  let explicitlySelectedColor = null;
+
+  this.addEventListener("colorSelected", ({ detail: { color } }) => {
+    explicitlySelectedColor = color;
+    console.log("colorSelected", color);
+  });
+
+  const createNode = ({ target, clientX: x, clientY: y }) => {
     if (globalIsDragging) return;
+    if (target.tagName !== "svg") return;
 
     const nodeId = ++maxNodeId;
     const shapeId = ++maxShapeId;
 
+    const w = window.innerWidth;
+    const h = window.innerHeight;
+    // const size = w > h ? h : w;
+    const a = (x / w - 0.5) * 0.8;
+    const b = (y / h - 0.5) * 0.8;
+    const color =
+      explicitlySelectedColor ??
+      new Color("oklab", [1, a, b]).toString({
+        format: "rgba",
+      });
+    console.log("create color", color);
+
     // Create a circle that controls the node
     const controllerShape = {
       type: "circle",
+      color,
       cx: x,
       cy: y,
       controlsNodeId: nodeId,
@@ -112,12 +141,14 @@ function* Svg({ nodes: initNodes = [], shapes: initShapes = [] }) {
     };
     console.log("create node", nodeId, node);
     nodes.set(nodeId, node);
+
+    this.refresh();
   };
 
-  window.addEventListener("pointerup", createNode);
+  window.addEventListener("pointerdown", createNode);
 
   while (true) {
-    requestAnimationFrame(() => this.refresh());
+    // requestAnimationFrame(() => this.refresh());
 
     for (let node of nodes.values()) {
       for (let { shapeId, attrs } of node.dependents) {
@@ -139,19 +170,25 @@ function* Svg({ nodes: initNodes = [], shapes: initShapes = [] }) {
     //   }
     // });
 
+    const sortedShapes = [...shapes.entries()].sort((a, b) =>
+      a.type === "line" && b.type === "circle" ? -1 : 1
+    );
+
     yield html`<svg
       viewBox="0 0 ${w} ${h}"
       style="width: ${w}px; height: ${h}px;"
       xmlns="http://www.w3.org/2000/svg"
     >
       <defs>${[...defs]}</defs>
-      ${[...shapes.entries()].map(([shapeId, shape]) => {
+      <${ColorWheel} w=${w} h=${h} />
+      ${sortedShapes.map(([shapeId, shape]) => {
         switch (shape.type) {
           case "circle":
             return svg`
               <${Orb} 
                 crank-key=${shapeId}
                 nodeId=${shape.controlsNodeId} 
+                color=${shape.color}
                 x=${shape.cx}
                 y=${shape.cy}
               /> 
@@ -173,7 +210,7 @@ function* Svg({ nodes: initNodes = [], shapes: initShapes = [] }) {
   }
 }
 
-function* Orb({ nodeId, x = 0, y = 0, r = 50 }) {
+function* Orb({ nodeId, x = 0, y = 0, r = 50, color }) {
   const pos = { x, y };
   let dragging = null;
 
@@ -217,58 +254,43 @@ function* Orb({ nodeId, x = 0, y = 0, r = 50 }) {
       cx=${pos.x}
       cy=${pos.y}
       r=${r}
-      fill="rgba(170, 170, 170, 0.5)"
-      stroke="rgba(200, 200, 200, 1)"
+      fill=${defaultColor}
+      stroke=${color ?? "rgba(200, 200, 200, 1)"}
       stroke-width="2"
     />`;
   }
 }
 
 function* Line({ x1, y1, x2, y2 }) {
-  // window.addEventListener("pointermove", () => {
-  //   if (!globalIsDragging) return;
-  //   this.refresh();
-  // });
-
   for ({ x1, y1, x2, y2 } of this) {
-    // console.log({ x1, y1, x2, y2 });
+    const dx = x2 - x1;
+    const dy = y2 - y1;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    const opacity =
+      1 -
+      1 / (1 + Math.pow(Math.E, (lineMaxDistance - distance) / lineTransition));
     yield svg`<line
       x1=${x1}
       y1=${y1}
       x2=${x2}
       y2=${y2}
-      stroke="rgba(200, 200, 200, 1)"
-      stroke-width="2"
+      stroke="rgba(240, 240, 240, ${opacity})"
+      stroke-width="3"
     />`;
   }
 }
 
 const n0 = {
   nodeId: 0,
-  x: 50,
-  y: 50,
+  x: window.innerWidth / 2,
+  y: window.innerHeight / 2,
   text: "n0",
   dependents: [
     { shapeId: 0, attrs: { x: "cx", y: "cy" } },
     { shapeId: 2, attrs: { x: "x1", y: "y1" } },
   ],
 };
-const n1 = {
-  nodeId: 1,
-  x: 100,
-  y: 120,
-  text: "n1",
-  dependents: [
-    { shapeId: 1, attrs: { x: "cx", y: "cy" } },
-    { shapeId: 2, attrs: { x: "x2", y: "y2" } },
-  ],
-};
 
 const s0 = { shapeId: 0, type: "circle", controlsNodeId: 0 };
-const s1 = { shapeId: 1, type: "circle", controlsNodeId: 1 };
-const s2 = { shapeId: 2, type: "line" };
 
-renderer.render(
-  html`<${Svg} nodes=${[n0, n1]} shapes=${[s0, s1, s2]} />`,
-  document.body
-);
+renderer.render(html` <${Svg} nodes=${[n0]} shapes=${[s0]} /> `, document.body);
