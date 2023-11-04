b9447cc196d26fe325885848efde3f6001502351
Duane Johnson <duane.johnson@gmail.com>
2023-11-03 20:28:56 -0600

    Add flow typechecking
    Experimenting with Flow, because it has typechecking within multiline
    comments built-in.

--- src/color.js
+++ src/color.js
@@ -1,25 +1,34 @@
+/* @flow */
 import Color from "colorjs.io";
+
 import { getScroll } from "./drag.js";
 
 const C = 0.8;
 
-export function getColorFromScreenCoord(x, y, w, h) {
+export function getColorFromScreenCoord(
+  x /*: number */,
+  y /*: number */,
+  w /*: number */,
+  h /*: number */
+) /*: any */ {
   const a = (x / w - 0.5) * C;
   const b = (y / h - 0.5) * C;
-  return new Color("oklab", [1, a, b]).toString({
-    format: "rgba",
-  });
+  return new Color("oklab", [1, a, b]).to("srgb").toString();
 }
 
-export function getColorFromPolarCoord(phi, lightness = 1.0) {
+export function getColorFromPolarCoord(
+  phi /*: number */,
+  lightness /*: number */ = 1.0
+) /*: any */ {
   const a = C * Math.cos(phi);
   const b = C * Math.sin(phi);
-  return new Color("oklab", [lightness, a, b]).toString({
-    format: "rgba",
-  });
+  return new Color("oklab", [lightness, a, b]).to("srgb").toString();
 }
 
-export function getColorFromWorldCoord(x, y) {
+export function getColorFromWorldCoord(
+  x /*: number */,
+  y /*: number */
+) /*: any */ {
   const { left, top } = getScroll();
   return getColorFromScreenCoord(
     x - left,
