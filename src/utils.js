6ec40a1cbdbc0f671289728394414621be6d901f
Duane Johnson <duane.johnson@gmail.com>
2023-11-07 06:20:58 -0700

    Add types to utils

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,21 @@
+// @flow
 import { jsx } from "@b9g/crank/standalone";
+import { Vector2 } from "./math/vector2.js";
 
 // Export jsx as both html and svg so that we get lit-html syntax highlighting
 export const html = jsx;
 export const svg = jsx;
 
-export function sigmoid(ratio) {
+export function sigmoid(ratio /*: number */) /*: number */ {
   return 1 - 1 / (1 + Math.pow(Math.E, ratio));
 }
 
 // Similar to sigmoid, but exact bounds [0, 1]; input x is [0, 1]
-export function squash(x, sharpness = 10) {
-  function g(x) {
+export function squash(
+  x /*: number */,
+  sharpness /*: number */ = 10
+) /*: number */ {
+  function g(x /*: number */) {
     return 1 / (1 + Math.exp(-sharpness * (x - 0.5)));
   }
 
@@ -20,25 +25,29 @@ export function squash(x, sharpness = 10) {
   return (g(x) - g0) / (g1 - g0);
 }
 
-function getCrankContext(component) {
+function getCrankContext(component /*: any */) {
   return component[Symbol.for("crank.ContextImpl")];
 }
 
 const IsSyncExecuting = 1 << 1;
-export function isSyncExecuting(component) {
+export function isSyncExecuting(component /*: any */) /*: boolean */ {
   return getCrankContext(component).f & IsSyncExecuting;
 }
 
 const IsUnmounted = 1 << 7;
-export function isUnmounted(component) {
+export function isUnmounted(component /*: any */) /*: boolean */ {
   return getCrankContext(component).f & IsUnmounted;
 }
 
-export function refresh(component) {
+export function refresh(component /*: any */) {
   if (!isSyncExecuting(component)) component.refresh();
 }
 
-export function dispatch(component, eventName /*: string */, detail) {
+export function dispatch(
+  component /*: any */,
+  eventName /*: string */,
+  detail /*: any */
+) {
   component.dispatchEvent(
     new CustomEvent(eventName, {
       bubbles: true,
@@ -55,22 +64,22 @@ type Side = {
 */
 
 export function closestSide(
-  x /*: number */,
-  y /*: number */,
-  w /*: number */,
-  h /*: number */
+  pos /*: Vector2 */,
+  size /*: Vector2 */
 ) /*: Side */ {
-  const ratio = w / h;
-  const diagFall = (w - x) / (h - y);
-  const diagRise = x / (h - y);
+  const ratio = size.width / size.height;
+  const diagFall = (size.width - pos.x) / (size.height - pos.y);
+  const diagRise = pos.x / (size.height - pos.y);
 
   if (diagFall < ratio && diagRise < ratio) {
-    return { side: "top", distance: y };
+    return { side: "top", distance: pos.y };
   } else if (diagFall < ratio && diagRise > ratio) {
-    return { side: "right", distance: w - x };
+    return { side: "right", distance: size.width - pos.x };
   } else if (diagFall > ratio && diagRise < ratio) {
-    return { side: "left", distance: x };
+    return { side: "left", distance: pos.x };
   } else if (diagFall > ratio && diagRise > ratio) {
-    return { side: "bottom", distance: h - y };
+    return { side: "bottom", distance: size.height - pos.y };
   }
+
+  throw new Error("impossible side");
 }
