eae495d5ba582ce5fbbb134868fb384c8799d332
Duane Johnson <duane.johnson@gmail.com>
2023-11-12 20:02:23 -0700

    Add types to math utils

--- src/math/utils.js
+++ src/math/utils.js
@@ -1,14 +1,29 @@
+// @flow
+
 export const DEG2RAD = Math.PI / 180;
 export const RAD2DEG = 180 / Math.PI;
 
-export function clamp(value, min, max) {
+export function clamp(
+  value /*: number */,
+  min /*: number */,
+  max /*: number */
+) /*: number */ {
   return Math.max(min, Math.min(max, value));
 }
 
-export function lerp(x, y, t) {
+export function lerp(
+  x /*: number */,
+  y /*: number */,
+  t /*: number */
+) /*: number */ {
   return (1 - t) * x + t * y;
 }
 
-export function damp(x, y, lambda, dt) {
+export function damp(
+  x /*: number */,
+  y /*: number */,
+  lambda /*: number */,
+  dt /*: number */
+) /*: number */ {
   return lerp(x, y, 1 - Math.exp(-lambda * dt));
 }
