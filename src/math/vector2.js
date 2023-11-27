28ceb962e0c38ffee221a7de259c0550a1b46280
Duane Johnson <duane.johnson@gmail.com>
2023-11-26 19:27:36 -0700

    Allow Vector2 to take {x,y} objects where possible

--- src/math/vector2.js
+++ src/math/vector2.js
@@ -2,6 +2,10 @@
 
 import { clamp } from "./utils.js";
 
+/*::
+type VecOrObj = Vector2 | { x: number, y: number, ... };
+*/
+
 export class Vector2 {
   x /*: number */;
   y /*: number */;
@@ -83,14 +87,14 @@ export class Vector2 {
     return new this.constructor(this.x, this.y);
   }
 
-  copy(v /*: Vector2 */) /*: Vector2 */ {
+  copy(v /*: VecOrObj */) /*: Vector2 */ {
     this.x = v.x;
     this.y = v.y;
 
     return this;
   }
 
-  add(v /*: Vector2 */) /*: Vector2 */ {
+  add(v /*: VecOrObj */) /*: Vector2 */ {
     this.x += v.x;
     this.y += v.y;
 
@@ -104,21 +108,21 @@ export class Vector2 {
     return this;
   }
 
-  addVectors(a /*: Vector2 */, b /*: Vector2 */) /*: Vector2 */ {
+  addVectors(a /*: VecOrObj */, b /*: VecOrObj */) /*: Vector2 */ {
     this.x = a.x + b.x;
     this.y = a.y + b.y;
 
     return this;
   }
 
-  addScaledVector(v /*: Vector2 */, s /*: number */) /*: Vector2 */ {
+  addScaledVector(v /*: VecOrObj */, s /*: number */) /*: Vector2 */ {
     this.x += v.x * s;
     this.y += v.y * s;
 
     return this;
   }
 
-  sub(v /*: Vector2 */) /*: Vector2 */ {
+  sub(v /*: VecOrObj */) /*: Vector2 */ {
     this.x -= v.x;
     this.y -= v.y;
 
@@ -132,14 +136,14 @@ export class Vector2 {
     return this;
   }
 
-  subVectors(a /*: Vector2 */, b /*: Vector2 */) /*: Vector2 */ {
+  subVectors(a /*: VecOrObj */, b /*: VecOrObj */) /*: Vector2 */ {
     this.x = a.x - b.x;
     this.y = a.y - b.y;
 
     return this;
   }
 
-  multiply(v /*: Vector2 */) /*: Vector2 */ {
+  multiply(v /*: VecOrObj */) /*: Vector2 */ {
     this.x *= v.x;
     this.y *= v.y;
 
@@ -153,7 +157,7 @@ export class Vector2 {
     return this;
   }
 
-  divide(v /*: Vector2 */) /*: Vector2 */ {
+  divide(v /*: VecOrObj */) /*: Vector2 */ {
     this.x /= v.x;
     this.y /= v.y;
 
@@ -164,32 +168,21 @@ export class Vector2 {
     return this.multiplyScalar(1 / scalar);
   }
 
-  // applyMatrix3(m /*: Matrix3 */) /*: Vector2 */ {
-  //   const x = this.x,
-  //     y = this.y;
-  //   const e = m.elements;
-
-  //   this.x = e[0] * x + e[3] * y + e[6];
-  //   this.y = e[1] * x + e[4] * y + e[7];
-
-  //   return this;
-  // }
-
-  min(v /*: Vector2 */) /*: Vector2 */ {
+  min(v /*: VecOrObj */) /*: Vector2 */ {
     this.x = Math.min(this.x, v.x);
     this.y = Math.min(this.y, v.y);
 
     return this;
   }
 
-  max(v /*: Vector2 */) /*: Vector2 */ {
+  max(v /*: VecOrObj */) /*: Vector2 */ {
     this.x = Math.max(this.x, v.x);
     this.y = Math.max(this.y, v.y);
 
     return this;
   }
 
-  clamp(min /*: Vector2 */, max /*: Vector2 */) /*: Vector2 */ {
+  clamp(min /*: VecOrObj */, max /*: VecOrObj */) /*: Vector2 */ {
     // assumes min < max, componentwise
 
     this.x = Math.max(min.x, Math.min(max.x, this.x));
@@ -213,28 +206,28 @@ export class Vector2 {
     );
   }
 
-  floor() /*: Vector2 */ {
+  floor() /*: VecOrObj */ {
     this.x = Math.floor(this.x);
     this.y = Math.floor(this.y);
 
     return this;
   }
 
-  ceil() /*: Vector2 */ {
+  ceil() /*: VecOrObj */ {
     this.x = Math.ceil(this.x);
     this.y = Math.ceil(this.y);
 
     return this;
   }
 
-  round() /*: Vector2 */ {
+  round() /*: VecOrObj */ {
     this.x = Math.round(this.x);
     this.y = Math.round(this.y);
 
     return this;
   }
 
-  roundToZero() /*: Vector2 */ {
+  roundToZero() /*: VecOrObj */ {
     this.x = Math.trunc(this.x);
     this.y = Math.trunc(this.y);
 
@@ -248,11 +241,11 @@ export class Vector2 {
     return this;
   }
 
-  dot(v /*: Vector2 */) /*: number */ {
+  dot(v /*: VecOrObj */) /*: number */ {
     return this.x * v.x + this.y * v.y;
   }
 
-  cross(v /*: Vector2 */) /*: number */ {
+  cross(v /*: VecOrObj */) /*: number */ {
     return this.x * v.y - this.y * v.x;
   }
 
@@ -291,17 +284,17 @@ export class Vector2 {
     return Math.acos(clamp(theta, -1, 1));
   }
 
-  distanceTo(v /*: Vector2 */) /*: number */ {
+  distanceTo(v /*: VecOrObj */) /*: number */ {
     return Math.sqrt(this.distanceToSquared(v));
   }
 
-  distanceToSquared(v /*: Vector2 */) /*: number */ {
+  distanceToSquared(v /*: VecOrObj */) /*: number */ {
     const dx = this.x - v.x,
       dy = this.y - v.y;
     return dx * dx + dy * dy;
   }
 
-  manhattanDistanceTo(v /*: Vector2 */) /*: number */ {
+  manhattanDistanceTo(v /*: VecOrObj */) /*: number */ {
     return Math.abs(this.x - v.x) + Math.abs(this.y - v.y);
   }
 
@@ -309,7 +302,7 @@ export class Vector2 {
     return this.normalize().multiplyScalar(length);
   }
 
-  lerp(v /*: Vector2 */, alpha /*: number */) /*: Vector2 */ {
+  lerp(v /*: VecOrObj */, alpha /*: number */) /*: Vector2 */ {
     this.x += (v.x - this.x) * alpha;
     this.y += (v.y - this.y) * alpha;
 
@@ -317,8 +310,8 @@ export class Vector2 {
   }
 
   lerpVectors(
-    v1 /*: Vector2 */,
-    v2 /*: Vector2 */,
+    v1 /*: VecOrObj */,
+    v2 /*: VecOrObj */,
     alpha /*: number */
   ) /*: Vector2 */ {
     this.x = v1.x + (v2.x - v1.x) * alpha;
@@ -327,7 +320,7 @@ export class Vector2 {
     return this;
   }
 
-  equals(v /*: Vector2 */) /*: boolean */ {
+  equals(v /*: VecOrObj */) /*: boolean */ {
     return v.x === this.x && v.y === this.y;
   }
 
@@ -348,14 +341,7 @@ export class Vector2 {
     return array;
   }
 
-  // fromBufferAttribute(attribute, index /*: number */) {
-  //   this.x = attribute.getX(index);
-  //   this.y = attribute.getY(index);
-
-  //   return this;
-  // }
-
-  rotateAround(center /*: Vector2 */, angle /*: number */) /*: Vector2 */ {
+  rotateAround(center /*: VecOrObj */, angle /*: number */) /*: Vector2 */ {
     const c = Math.cos(angle),
       s = Math.sin(angle);
 
